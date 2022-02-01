4a0ed8dc6dd6b68b05bd9b117f3a037c96042c58
teresa knowlton <mtknowlton67@gmail.com>
2022-02-01 14:55:52 -0800

    added a click event and a museenter event

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import './less/index.less'
 
 // Your code goes here!
-const busImage = document.querySelector('.intro img');
-busImage.addEventListener('click', function(e){
-    console.log(e);
+
+
+//const busImage = document.querySelector('.intro');
+
+const nav = document.querySelector('.nav-link');
+
+function random(number) {
+    return Math.floor(Math.random() * (number+1));
+  }
+nav.addEventListener('click', () => {
+    
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
 })
+
+
+// busImage.addEventListener('mouseenter', (event) => {
+//  busImage.style.transform = "scale(.6)";
+//  busImage.style.transition ="transform 1s";
+// })
+// busImage.addEventListener('mouseleave', () => {
+//     busImage.style.transform = "scale(1)";
+// })
+
+// const logoHeading = documnet.querySelector('.logo-heading');
+
+// logoHeading.addEventListener('mouseenter', (event) => {
+//     logoHeading.style.color = 'hotpink';
+// })
