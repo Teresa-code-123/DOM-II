3a0b0975697a753fcf04990d8776aba145ca256b
teresa knowlton <mtknowlton67@gmail.com>
2022-02-01 15:48:08 -0800

    Added a click event

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import './less/index.less'
 // Your code goes here!
 
 
-//const busImage = document.querySelector('.intro');
+const busImage = document.querySelector('.intro img');
 
 const nav = document.querySelector('.nav-link');
+
+const changeImage = document.querySelector('logo-heading ')
+
 
 function random(number) {
     return Math.floor(Math.random() * (number+1));
@@ -13,20 +16,15 @@ function random(number) {
 nav.addEventListener('click', () => {
     
     const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-  document.body.style.backgroundColor = rndCol;
+  document.body.style.color = rndCol;
 })
 
 
-// busImage.addEventListener('mouseenter', (event) => {
-//  busImage.style.transform = "scale(.6)";
-//  busImage.style.transition ="transform 1s";
-// })
-// busImage.addEventListener('mouseleave', () => {
-//     busImage.style.transform = "scale(1)";
-// })
+busImage.addEventListener('mouseenter', (event) => {
+ busImage.style.transform = "scale(.6)";
+ busImage.style.transition ="transform 1s";
+})
+busImage.addEventListener('mouseleave', () => {
+    busImage.style.transform = "scale(1)";
+})
 
-// const logoHeading = documnet.querySelector('.logo-heading');
-
-// logoHeading.addEventListener('mouseenter', (event) => {
-//     logoHeading.style.color = 'hotpink';
-// })
