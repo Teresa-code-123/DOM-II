cc054f4a56e231f89ad2f1305538f4648d180f4d
teresa knowlton <mtknowlton67@gmail.com>
2022-02-01 09:10:15 -0800

    Made an event handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './less/index.less'
 
 // Your code goes here!
+const busImage = document.querySelector('.intro img');
+busImage.addEventListener('click', function(e){
+    console.log(e);
+})
